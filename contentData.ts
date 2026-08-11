2775ecc992c7486686de7cedcac03925c657251a
Martin <mcsato@seznam.cz>
2026-08-12 00:34:04 +0200

    Integruje plné interaktivní vizualizace pro projekty Čerpadlo, Gravitační Hřiště, Celulární Automat, Kompenzace a Vetknutý Nosník

--- contentData.ts
+++ contentData.ts
@@ -1,7 +1,7 @@
 import { ContentDetail } from './types';
 
 /**
- * Hlavní konfigurace položek navigačního menu aplikace.
+ * Hlavní konfigurace položek navigačního menu aplikace s integrovanými vizualizacemi.
  */
 export const CONTENT_ITEMS: ContentDetail[] = [
   {
@@ -26,45 +26,45 @@ export const CONTENT_ITEMS: ContentDetail[] = [
     additionalInfo: "Samostatná webová kalkulačka a nástroj pro elektroinženýrské výpočty zkratových proudů."
   },
   {
+    id: "cerpadlo",
+    name: "Čerpadlo",
+    description: "Výpočet a interaktivní vizualizace charakteristik čerpadla.",
+    url: "./apps/Cerpadlo/index.html",
+    additionalInfo: "Kompletní interaktivní výpočtová aplikace s vizualizací pracovních bodů čerpadla, průtoku a hydraulických vlastností."
+  },
+  {
     id: "gravitacni-hriste",
     name: "Gravitační Hřiště",
     description: "Simulace gravitačního pole a fyzikálních částic.",
-    url: "https://github.com/Martin74Cs/GravitacniHriste",
-    additionalInfo: "Projekt na GitHubu zaměřený na fyzikální simulace gravitačních sil a pohybu objektů v prostoru."
-  },
-  {
-    id: "cerpadlo",
-    name: "Čerpadlo",
-    description: "Výpočet a simulace parametrů čerpadla.",
-    url: "https://github.com/Martin74Cs/Cerpadlo",
-    additionalInfo: "Projekt pro výpočty a modelování charakteristik čerpadel, hydraulických ztrát a průtoku kapalin."
+    url: "./apps/GravitacniHriste/index.html",
+    additionalInfo: "Interaktivní fyzikální simulátor gravitačních sil a pohybu oběžných těles."
   },
   {
     id: "celularni-automat",
     name: "Celulární Automat",
     description: "Simulace celulárních automatů a mřížkových systémů.",
-    url: "https://github.com/Martin74Cs/CelularniAutomat",
-    additionalInfo: "Projekt zaměřený na celulární automaty (např. Conway's Game of Life, pravidlové mřížkové automaty)."
+    url: "./apps/CelularniAutomat/index.html",
+    additionalInfo: "Interaktivní modul pro simulace celulárních automatů a pravidlových mřížkových struktur."
   },
   {
     id: "kompenzace",
     name: "Kompenzace",
     description: "Výpočty kompenzace účiníku a jalového výkonu.",
-    url: "https://github.com/Martin74Cs/Kompenzace",
-    additionalInfo: "Elektrotechnické výpočty pro dimenzování kompenzačních rozvaděčů a kondenzátorových baterií."
+    url: "./apps/Kompenzace/index.html",
+    additionalInfo: "Elektrotechnická výpočtová aplikace pro dimenzování kompenzačních rozvaděčů a kondenzátorových baterií."
   },
   {
     id: "vetknuty-nosnik",
     name: "Vetknutý Nosník",
     description: "Výpočet ohybu, napětí a statického posouzení vetknutého nosníku.",
-    url: "https://github.com/Martin74Cs/VetknutyNosnik",
-    additionalInfo: "Inženýrský výpočtový modul pro statické a pevnostní posouzení vetknutého nosníku."
+    url: "./apps/VetknutyNosnik/index.html",
+    additionalInfo: "Inženýrský výpočtový modul s grafikou ohybových momentů a posouzení vetknutého nosníku."
   },
   {
-    id: "help",
-    name: "Nápověda & Odkazy",
-    description: "Informace o používání webu a návody.",
-    imageUrl: "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?auto=format&fit=crop&w=800&q=80",
-    additionalInfo: "Veškeré veřejné odkazy naleznete v levém menu. Pro zobrazení poznámek z repozitáře Obsidian vyberte záložku Obsidian Poznámky."
+    "id": "help",
+    "name": "Nápověda & Odkazy",
+    "description": "Informace o používání webu a návody.",
+    "imageUrl": "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?auto=format&fit=crop&w=800&q=80",
+    "additionalInfo": "Veškeré veřejné odkazy a interaktivní kalkulátory naleznete v levém menu."
   }
 ];
